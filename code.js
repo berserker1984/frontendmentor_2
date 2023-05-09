cba46bb9cf70fb38f433c43378415c419f22a1f4
Reaper <laerte.mancio@gmail.com>
2023-05-09 20:51:03 -0300

    mobile-ok

---
+++ code.js
@@ -0,0 +1,6 @@
+
+let elemento = document.querySelectorAll('input, label, #tela1, #tela2,#rating-inserir') 
+let elemento1 = document.querySelectorAll('label')
+let elemento2 = document.querySelectorAll('input')
+
+
